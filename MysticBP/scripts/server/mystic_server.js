6bd514d6fe299f64dc558e172072db0fbb33abbb
Sprintermax <ericfnoe7@hotmail.com>
2019-01-15 00:35:14 -0200

    Updated Forcefield Script
    Added Multiples Forcefield Detection

--- MysticBP/scripts/server/mystic_server.js
+++ MysticBP/scripts/server/mystic_server.js
@@ -7,14 +7,24 @@ system.initialize = function() {
 
 system.update = function () {
 	global.tick += 1;
-    if (global.tick === 5) {
+    if (global.tick == 5) {
         let allEntities = system.getEntitiesFromQuery(global.viewAllEntities);
         let size = allEntities.length;
         for (let index = 0; index < size; ++index) {
-			if (allEntities[index].__identifier__ === "mystic:forcefield") {
+			if (allEntities[index].__identifier__ == "mystic:forcefield") {
 				let checkActivated = this.getComponent(allEntities[index], "minecraft:health");
-				if (checkActivated.max === 50) {
-					this.broadcastEvent("minecraft:execute_command", "/say Activated :P");
+				let forcefieldpos = this.getComponent(allEntities[index], "minecraft:position");
+				if (checkActivated.max == 50) {
+					this.broadcastEvent("minecraft:execute_command", "/tellraw @a {\"rawtext\":[{\"text\":\"§d<MythicalExpansion> §aActivated Forcefield At: §rX: "+forcefieldpos.x+", Y: "+forcefieldpos.y+", Z: "+forcefieldpos.z+"\"}]}");
+					checkActivated.max = 55;
+					this.applyComponentChanges(allEntities[index], checkActivated);
+				} else if (checkActivated.max == 55) {
+					this.broadcastEvent("minecraft:execute_command", "/tellraw @a {\"rawtext\":[{\"text\":\"§d<MythicalExpansion> §2Working...\"}]}");
+				}
+				if (checkActivated.max == 25) {
+					this.broadcastEvent("minecraft:execute_command", "/tellraw @a {\"rawtext\":[{\"text\":\"§d<MythicalExpansion> §aDeactivated Forcefield At: §rX: "+forcefieldpos.x+", Y: "+forcefieldpos.y+", Z: "+forcefieldpos.z+"\"}]}");
+					checkActivated.max = 20;
+					this.applyComponentChanges(allEntities[index], checkActivated);
 				}
             }
         }
